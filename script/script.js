cf9ca1b8aefcd27e005fb268f67fa424db61a2a7
PedroZanette <phzanette@gmail.com>
2024-08-19 20:09:15 -0300

    Version With Default Values

--- script/script.js
+++ script/script.js
@@ -75,4 +75,86 @@ escopoAninhado();
 
 console.log(m);
 
+// 5 - arrow function
+const testArrow = () => {
+    console.log("Esta é uma arrow function")
+}
 
+testArrow();
+
+const parOuImpar = (n) => {
+    if(n % 2 === 0) {
+        console.log("Par");
+        return;
+    }
+
+    console.log("Impar");
+}
+
+parOuImpar(5);
+
+parOuImpar(10);
+
+//6 - mais sobre arrow functions
+const raizQuadrada = (x) => {
+    return x * x;
+}
+
+console.log(raizQuadrada(4));
+
+const raizQuadrada2 = (x) => x * x
+
+console.log(raizQuadrada2(5));
+
+console.log(raizQuadrada2(12));
+
+const helloWorld = () => console.log("Hellow World")
+
+helloWorld();
+
+
+// 7 - parametro opcional
+
+const multiplication = function(m, n){
+
+    if(n === undefined) {
+        return m * 2;
+    } else{
+        return m * n;
+    }
+}
+
+console.log(multiplication(5));
+
+console.log(multiplication(4, 6));
+
+const greeting = (name) => {
+
+    if(!name){
+        console.log("Olá!")
+    }
+    console.log("Olá " + name + "!")
+}
+
+greeting();
+
+greeting("Pedro");
+
+// 8 - valor default
+const customGreeting = (name, greet = "Olá") => {
+    return `${greet}, ${name}!`
+}
+
+console.log(customGreeting("Pedro"));
+
+console.log(customGreeting("Zanette", "Bom dia"));
+
+const repeatText = (text, repeat = 2) => {
+    for(let i = 0; i < repeat; i++){
+        console.log(text);
+    }
+};
+
+repeatText("Testando");
+
+repeatText("Repetindo 6 vezes", 6)
